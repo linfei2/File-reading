3c53846bd47e991860dbeecca7955035201c74bf
linfei2 <dominika.matczynska@gmail.com>
2022-03-17 20:24:59 +0100

    Used Counter to determine number of images in each category

--- zadanie22b.py
+++ zadanie22b.py
@@ -1,17 +1,19 @@
-with open('Training_01.txt', 'r') as open_file:
-    text = open_file.read()
+from collections import Counter
 
-paths = text.split('\n')
 
-paths_elements = [path.split('/') for path in paths]
+def main():
+    with open('Training_01.txt', 'r') as open_file:
+        lines = open_file.readlines()
+        number_of_images = len(lines)
+        line_elements = [line.split('/') for line in lines]
+        categories = [line_elements[i][2] for i in range(len(line_elements)-1)]
+        number_of_categories = len(set(categories))
+        images_per_category = Counter(categories)
+        print(f"There are {number_of_images} images in {number_of_categories} categories.")
+        print(f"Number of images in each category:")
+        for category, count in images_per_category.items():
+            print(f"{category}: {count}")
 
-index2_elements = [paths_elements[i][2] for i in range(len(paths_elements)-1)]
 
-categories = list(dict.fromkeys(index2_elements))
-
-cat_img_num = {i:index2_elements.count(i) for i in index2_elements}
-
-print()
-print('There are ', str(len(paths)-1), ' images in ', str(len(categories)), 'categories.', '\n')
-print('Number of images in each category can be found below:', '\n')
-print('\n'.join('{}: {}'.format(k, v) for k, v in cat_img_num.items()))
+if __name__ == "__main__":
+    main()
