80ab716a71d460ab173eb618bb0a7626836e3a1b
linfei2 <dominika.matczynska@gmail.com>
2022-03-18 19:24:45 +0100

    Changed to reading line by line

--- zadanie22b.py
+++ zadanie22b.py
@@ -3,10 +3,13 @@ from collections import Counter
 
 def main():
     with open('Training_01.txt', 'r') as open_file:
-        lines = open_file.readlines()
-        number_of_images = len(lines)
-        line_elements = [line.split('/') for line in lines]
-        categories = [line_elements[i][2] for i in range(len(line_elements)-1)]
+        line = open_file.readline()
+        line_elements = []
+        while line:
+            line_elements.append(line.split('/'))
+            line = open_file.readline()
+        number_of_images = len(line_elements)
+        categories = [line_elements[i][2] for i in range(len(line_elements))]
         number_of_categories = len(set(categories))
         images_per_category = Counter(categories)
         print(f"There are {number_of_images} images in {number_of_categories} categories.")
